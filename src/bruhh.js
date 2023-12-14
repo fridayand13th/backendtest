b149010e76073b936f9ce7e4ac40a09d2a10c25f
fridayand13th <nguyentienmanh003@gmail.com>
2023-12-15 00:16:28 +0700

    h

--- src/bruhh.js
+++ src/bruhh.js
@@ -1,11 +1,19 @@
+//#20
 const express = require('express')
 const app = express()
 const port = 3000
+const path =require('path')
+
+app.set('views', path.join(__dirname,'views'))
+app.set('view engine','ejs')
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/friday', (req, res) => {
+    res.render('vd.ejs')
+  })
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
